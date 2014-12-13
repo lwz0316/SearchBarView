f9994b877e67114ff16f16d18c8fbbeded4dd6ab
lwz <lwz0316@gmail.com>
2014-12-13 21:11:43 +0800

    add LICENSE

--- SearchBarDemo/src/com/lwz/searchbar/demo/AbsViewHolderAdapter.java
+++ SearchBarDemo/src/com/lwz/searchbar/demo/AbsViewHolderAdapter.java
@@ -1,3 +1,24 @@
+/*******************************************************************************
+* Copyright 2014, 2015 Liu Wenzhu
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+ *******************************************************************************/
 package com.lwz.searchbar.demo;
 
 import java.util.ArrayList;
@@ -17,10 +38,6 @@ import android.widget.TextView;
  * @param <T>
  * 
  * @author Liu Wenzhu
- * @mail liu.wenzhu@rytong.com
- *
- * Copyright ® 2014 北京融易通信息技术有限公司
- * 版权所有。
  *
  */
 public abstract class AbsViewHolderAdapter<T> extends BaseAdapter {
